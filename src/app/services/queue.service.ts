85b4ea5a7baa3c0b72bb09c0291a2452a466941b
Daniel <daniel001.dhf@gmail.com>
2024-09-12 15:33:22 +0800

    Added Priority Queue Logic

--- src/app/services/queue.service.ts
+++ src/app/services/queue.service.ts
@@ -1,34 +1,43 @@
 import { Injectable } from '@angular/core';
-import { BotService } from './bot.service';
-import { OrderService } from './order.service';
-import { filter, from, groupBy, mergeMap, Observable, withLatestFrom } from 'rxjs';
-import { Bot, Order } from '../models';
+import { BehaviorSubject, combineLatest, map, startWith } from 'rxjs';
+import { Queue } from '../models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QueueService {
 
-  bots$!: Observable<Bot[]>;
-  orders$!: Observable<Order[]>;
-  botCount$!: Observable<number>;
+  constructor() { }
 
-  botCounter = 1;
+  _vipQueue = new BehaviorSubject<Queue[]>([]);
+  _normalQueue = new BehaviorSubject<Queue[]>([]);
 
-  constructor(
-    private _botService: BotService,
-    private _orderService: OrderService
-  ) {
-    this.bots$ = this._botService.bots$;
-    this.orders$ = this._orderService.orders$;
-    this.botCount$ = this._botService.botCount$;
-   }
+  vipQueue$ = this._vipQueue.asObservable();
+  normalQueue$ = this._normalQueue.asObservable();
 
-  queue$ = from(this.orders$).pipe(
-    filter(order => !!order),
-    withLatestFrom(this.botCount$),
-    groupBy(([order, botCount]) => this.botCounter++ % botCount),
-    //think about this later
+  priorityQueue$ = combineLatest([
+    this.vipQueue$.pipe(startWith([])),
+    this.normalQueue$.pipe(startWith([])),
+  ]).pipe(
+    map(([vipOrders, normalOrders]) => [...vipOrders, ...normalOrders]),
   )
 
+  _processedQueue = new BehaviorSubject<Queue[]>([]);
+  processedQueue$ = this._processedQueue.asObservable();
+
+  addQueue(order: Queue) {
+    if (order.vip) {
+      this._vipQueue.next([...this._vipQueue.getValue(), order]);
+    } else {
+      this._normalQueue.next([...this._normalQueue.getValue(), order]);
+    }
+  }
+
+  removeQueue(order: Queue) {
+    if (order.vip) {
+      this._vipQueue.next(this._vipQueue.getValue().filter(o => o.id !== order.id));
+    } else {
+      this._normalQueue.next(this._normalQueue.getValue().filter(o => o.id !== order.id));
+    }
+  }
 }
